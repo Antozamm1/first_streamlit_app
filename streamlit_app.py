0680251921935ca8007c9e33202285861e7664f5
Antozamm1 <134511736+Antozamm1@users.noreply.github.com>
2023-08-31 08:24:57 +0200

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ streamlit.dataframe(my_data_row)
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-if add_my_fruit is not '':
+if add_my_fruit != '':
   my_cur.execute(f"insert into fruit_load_list(fruit_name) values ('{add_my_fruit}');")
   streamlit.write('Thanks for adding ', add_my_fruit)
 
